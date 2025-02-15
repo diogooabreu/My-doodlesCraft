aa6430fcc3c970b3ef60ec979dc4ababcfe88bce
Diogo Abreu <abreudiogo177@gmail.com>
2025-02-14 22:02:55 -0300

    O primeiro

---
+++ DoodlesCraft/src/app/components/modal-details/modal-details.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-details',
+  standalone: true,
+  imports: [],
+  templateUrl: './modal-details.component.html',
+  styleUrl: './modal-details.component.css'
+})
+export class ModalDetailsComponent {
+
+}
